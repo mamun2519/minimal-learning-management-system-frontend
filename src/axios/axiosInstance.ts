29d3a050cb0a39d589dca84db6f98b4c4e9f5e3d
mamun2519 <juborajislam46@gmail.com>
2025-08-16 14:45:39 +0600

    Feat: added to user slice

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -1,8 +1,4 @@
-// import { authKey } from "@/constants/storageKey";
-// import { IGenericErrorResponse, ResponseSuccessType } from "@/types";
-// import { getFromLocalStorage } from "@/utils/local-storage";
 import { authKey } from "@/constants/storageKey";
-import { IGenericErrorResponse, ResponseSuccessType } from "@/types";
 import { getFromLocalStorage } from "@/utils/local-storage";
 import axios from "axios";
 
