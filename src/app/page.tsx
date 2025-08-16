29bdfaeb11dd9965b8d22e08865ca4057f0330d6
mamun2519 <juborajislam46@gmail.com>
2025-08-16 15:22:27 +0600

    Feat: modify to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 import InToCourse from "@/components/course/InToCourse";
-import { URL } from "@/constants/url";
-// import "tw-animate-css";
 
-async function Home() {
-  const data = await fetch(`${URL}/course`, {
-    // next: { revalidate: 60 },
-  });
-  const result = await data.json();
-  const courses = result?.data?.data;
-  const coursesMetaData = result?.data.meta;
-  console.log(courses);
+function Home() {
   return (
     <div>
-      <InToCourse courses={courses} coursesMetaData={coursesMetaData} />
+      <InToCourse />
     </div>
   );
 }
