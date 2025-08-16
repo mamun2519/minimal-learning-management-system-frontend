5ea170cc1d5454e559c1b6750fa5628dbde0cc36
mamun2519 <juborajislam46@gmail.com>
2025-08-16 13:00:30 +0600

    Feat: connect to course data page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import InToCourse from "@/components/course/InToCourse";
-import "tw-animate-css";
+import { URL } from "@/constants/url";
+// import "tw-animate-css";
 
-export default function Home() {
+async function Home() {
+  const data = await fetch(`${URL}/course`, {
+    // next: { revalidate: 60 },
+  });
+  const result = await data.json();
+  const courses = result?.data?.data;
+  const coursesMetaData = result?.data.meta;
+  console.log(coursesMetaData);
   return (
     <div>
       <InToCourse />
     </div>
   );
 }
+
+export default Home;
