b3c14757350a87f72ca3c33c4acf1415982ab8eb
mamun2519 <juborajislam46@gmail.com>
2025-08-16 12:17:00 +0600

    Modify: modify to search page

--- src/components/ui/Searchbar.tsx
+++ src/components/ui/Searchbar.tsx
@@ -1,4 +1,3 @@
-import { Input } from "@mui/material";
 import { Search } from "lucide-react";
 
 interface SearchBarProps {
@@ -14,15 +13,15 @@ function SearchBar({
 }: SearchBarProps) {
   return (
     <div className="relative max-w-md mx-auto">
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <Search className="h-5 w-5 text-muted-foreground" />
+      <div className="absolute  left-0 top-2.5 pl-3 flex items-center ">
+        <Search className="h-5 w-5 text-black " />
       </div>
-      <Input
+      <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
-        className="pl-10 pr-4 py-2 w-full border border-border rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent"
+        className="pl-10 pr-4 py-2 w-full border  rounded-lg  bg-white"
       />
     </div>
   );
