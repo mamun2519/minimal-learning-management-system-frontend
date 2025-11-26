e42b222a8d21f64137e3698c9e220889bc748c0e
mamun2519 <juborajislam46@gmail.com>
2025-11-26 20:01:33 +0600

    change to env file

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,4 @@
 // export const URL = "http://localhost:5000/api/v1/";
 // export const URL =
 //   "https://minimal-learning-management-system.vercel.app/api/v1/";
-export const URL = process.env.REACT_APP_API_URL;
+export const URL = process.env.NEXT_PUBLIC_API_URL;
