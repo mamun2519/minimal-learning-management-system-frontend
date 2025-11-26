a7aa35888c4fd6950eb54171a8a4e48602944c2d
mamun2519 <juborajislam46@gmail.com>
2025-11-26 18:54:37 +0600

    change to backend url for kubernetes deployment

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,4 @@
-export const URL = "http://localhost:5000/api/v1/";
+// export const URL = "http://localhost:5000/api/v1/";
 // export const URL =
 //   "https://minimal-learning-management-system.vercel.app/api/v1/";
+export const URL = process.env.REACT_APP_API_URL;
